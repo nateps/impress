f4eb70f6dfb9fc5039287da71512d9b83bac02ec
nateps <nate@nateps.com>
2011-08-09 01:27:15 -0700

    Remove non-static files

--- public/vogue.js
+++
@@ -1,5 +0,0 @@
-var loc = window.location,
-    path = loc.protocol + '//' + loc.hostname + ':8002/vogue-client.js',
-    script = document.createElement('script')
-script.setAttribute('src', path)
-document.body.appendChild(script)
